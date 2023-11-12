091d4e8183fc88054cd383fcba8ea231880945af
taeyoung kang <kty1467@naver.com>
2023-11-12 17:07:15 +0900

    일부

---
+++ src/main/java/com/iot/travel/dto/LoginRequestDTO.java
@@ -0,0 +1,9 @@
+package com.iot.travel.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginRequestDTO {
+    private String username;
+    private String password;
+}
--- src/main/java/com/iot/travel/repository/UserRepository.java
+++ src/main/java/com/iot/travel/repository/UserRepository.java
@@ -4,4 +4,6 @@ import com.iot.travel.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
 }
